c57d905e087085b75d3d1f0a069a55f1baba1119
mayapriati <a710230052@student.ums.ac.id>
2024-06-08 14:40:37 +0700

    day 2

---
+++ day2/day2.py
@@ -0,0 +1,6 @@
+Nama = input ("Siapa Nama Kamu? ")
+print ("Saya", Nama)
+NIM = input ("Berapa NIM Kamu? ")
+print ("NIM Saya", NIM)
+Angkatan = input ("Angkatan Berapa? ")
+print ("Saya Angkatan", Angkatan)
