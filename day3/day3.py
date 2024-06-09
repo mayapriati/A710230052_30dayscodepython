48319bbd0a24ac5292a9f9cf3cde35f6061549a0
mayapriati <a710230052@student.ums.ac.id>
2024-06-09 15:12:47 +0700

    dat 3

---
+++ day3/day3.py
@@ -0,0 +1,8 @@
+#input jarak dalam kilometer
+jarak_km = int(input('masukkan jarak dalam kilometer: '))
+
+#konversi kilometer menjadi meter
+jarak_meter = jarak_km*1000 
+
+#menampilkan hasil konversi
+print('hasil konversi dari kilometer menjadi meter adalah', jarak_meter,'m')
